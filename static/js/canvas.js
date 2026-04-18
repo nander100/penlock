f27ff832cec906d8d0906664b4e2a81b70c60a72
pilafnoodle <amberlin618@gmail.com>
2026-04-18 11:42:38 -0700

    w3schools code

---
+++ static/js/canvas.js
@@ -0,0 +1,88 @@
+//This is taken from w3schools
+
+// wait for the content of the window element
+// to load, then performs the operations.
+// This is considered best practice.
+window.addEventListener('load', () => {      
+    resize(); // Resizes the canvas once the window loads
+    document.addEventListener('mousedown', startPainting);
+    document.addEventListener('mouseup', stopPainting);
+    document.addEventListener('mousemove', sketch);
+
+    document.addEventListener('touchstart', startPainting);
+    document.addEventListener('touchend', stopPainting);
+    document.addEventListener('touchmove', sketch);
+
+    window.addEventListener('resize', resize);
+});
+  
+const canvas = document.querySelector('#canvas');
+ 
+// Context for the canvas for 2 dimensional operations
+const ctx = canvas.getContext('2d');
+  
+// Resizes the canvas to the available size of the window.
+function resize(){
+  ctx.canvas.width = 1000 ;
+  ctx.canvas.height = 300;
+}
+  
+// Stores the initial position of the cursor
+let coord = {x:0 , y:0}; 
+ 
+// This is the flag that we are going to use to 
+// trigger drawing
+let paint = false;
+  
+// Updates the coordianates of the cursor when 
+// an event e is triggered to the coordinates where 
+// the said event is triggered.
+function getPosition(event) {
+    // if touch, use the first touch point
+    if (event.touches) {
+        coord.x = event.touches[0].clientX - canvas.offsetLeft;
+        coord.y = event.touches[0].clientY - canvas.offsetTop;
+    } else {
+        coord.x = event.clientX - canvas.offsetLeft;
+        coord.y = event.clientY - canvas.offsetTop;
+    }
+}
+
+// The following functions toggle the flag to start
+// and stop drawing
+function startPainting(event){
+  paint = true;
+  getPosition(event);
+}
+function stopPainting(){
+  paint = false;
+}
+  
+function sketch(event){
+  if (!paint) return;
+  ctx.beginPath();
+  
+  ctx.lineWidth = 5;
+ 
+  // Sets the end of the lines drawn
+  // to a round shape.
+  ctx.lineCap = 'round';
+  
+  ctx.strokeStyle = 'black';
+    
+  // The cursor to start drawing
+  // moves to this coordinate
+  ctx.moveTo(coord.x, coord.y);
+ 
+  // The position of the cursor
+  // gets updated as we move the
+  // mouse around.
+  getPosition(event);
+ 
+  // A line is traced from start
+  // coordinate to this coordinate
+  ctx.lineTo(coord.x , coord.y);
+  
+  // Draws the line.
+  ctx.stroke();
+}
